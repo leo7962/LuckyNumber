3a5e126bd8491492af8999f25c8cbf9cf991e5b1
leo7962 <ingenieroleonardo@outlook.com>
2020-08-13 17:29:21 -0500

    Modificación de clase

--- LuckyNumber/MainActivity.cs
+++ LuckyNumber/MainActivity.cs
@@ -1,7 +1,6 @@
 ﻿using Android.App;
 using Android.OS;
 using Android.Support.V7.App;
-using Android.Runtime;
 using Android.Widget;
 using System;
 
@@ -10,19 +9,19 @@ namespace LuckyNumber
     [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true, Icon = "@mipmap/First_Icon")]
     public class MainActivity : AppCompatActivity
     {
-        SeekBar seekBar;
-        TextView resultTextView;
-        Button rollButton;
+        private SeekBar seekBar;
+        private TextView resultTextView;
+        private Button rollButton;
         protected override void OnCreate(Bundle savedInstanceState)
         {
             base.OnCreate(savedInstanceState);
-            
+
             // Set our view from the "main" layout resource
             SetContentView(Resource.Layout.activity_main);
             seekBar = FindViewById<SeekBar>(Resource.Id.seekBar);
             resultTextView = FindViewById<TextView>(Resource.Id.resultTextView);
-            resultTextView.Text = "9";
-            rollButton = (Button)FindViewById<Button>(Resource.Id.rollButton);
+            resultTextView.Text = "10";
+            rollButton = FindViewById<Button>(Resource.Id.rollButton);
             rollButton.Click += RollButton_Click;
 
             SupportActionBar.Title = "My Lucky Number App";
